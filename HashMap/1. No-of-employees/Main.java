2d487e5be3c619ef7495095b1de686f4b402b78b
Romesh Yadav <romesh.yadav@jumbaya.com>
2025-04-08 12:19:07 +0530

    Hashmap no of employee

---
+++ HashMap/1. No-of-employees/Main.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void countEmployees(HashMap<Character, Character> map) {
+        HashMap<Character, Integer> res = new HashMap<>();
+        for(Map.Entry<Character,Character> entry: map.entrySet()){
+            
+        }
+    }
+
+    public static void main(String[] args) {
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put('A', 'B');
+        map.put('B', 'C');
+        map.put('C', 'D');
+        map.put('F', 'D');
+        map.put('G', 'D');
+
+
+    }
+}
---
+++ HashMap/notes.md
@@ -0,0 +1,1 @@
+1. Number of Employees under every Manager
